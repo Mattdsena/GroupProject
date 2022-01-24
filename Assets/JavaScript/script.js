e07fbe6d9f57f9d9163617b107e7223cf974e35b
Hannah <94648556+hannahj25@users.noreply.github.com>
2022-01-24 18:45:25 +1030

    pseudocode js file

---
+++ Assets/JavaScript/script.js
@@ -0,0 +1,25 @@
+// Select elements from html and assign to variables
+// var wordBtn = document.querySelector("");
+
+
+// Assign variable to  random word api url
+//var wordDef = 'https://wordsapiv1.p.mashape.com/words/definitions?random=true'
+//var wordSyn = 'https://wordsapiv1.p.mashape.com/words/synonyms?random=true'
+//var wordEx = 'https://wordsapiv1.p.mashape.com/words/examples?random=true'
+
+
+// Function to fetch word apis and display to page
+  // create new elements, assign text content as fetch responses
+  // append new elements 
+
+
+
+
+// Function to fetch sounds
+
+
+
+
+
+// on click wordBtn, call fetch word function
+
